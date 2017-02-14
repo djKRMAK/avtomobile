3bd75ea03e599a702f282c20fe6bfcdcbb8cd918
Leonardo Zec <dolantehduk1@gmail.com>
2017-02-14 18:43:16 +0100

    POpravili formu extra

--- brands_new.php
+++ brands_new.php
@@ -61,16 +61,27 @@
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_content">
-                    <h1>Unos proivodzacha</h1>
+                    <h1>Unos proizvodzacha</h1>
 
-                  
-                <form action = "brands_create.php" method="post">
-                ime proizvodzacha:
-                <input type="text" name="name"><br>
-                datum osnutka:
-                <input type="text" name="year_est"><br>
-                <input type="submit" value="unesi">
-
+                             
+                
+                <form action="brands_create.php" method="post" class="form-horizontal form-label-left" >
+                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Ime Proizvodzacha: <span class="required"></span>
+                </label>
+                <div class=form-group>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                <input type="text" id="name" name="name" required="required" class="form-control col-md-7 col-xs-12"></div>
+                </div>
+                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="year_est">Datum osnutka: <span class="required"></span>
+                </label>
+                <div class=form-group>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                <input type="text" id="name" name="year_est" required="required" class="form-control col-md-7 col-xs-12"></div>             
+                </div>
+                <div class=form-group>
+                <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                <button type="submit" value="unesi" class="btn btn-success">Unesi</button>
+                </div>
                 </form>
                   </div>
                 </div>
