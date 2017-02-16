38c9a55d1dab76360f40ee87c585185140789230
Leonardo Zec <dolantehduk1@gmail.com>
2017-02-16 11:24:57 +0100

    sad radi bratex

--- brands_create.php
+++ brands_create.php
@@ -1,8 +1,7 @@
 <?php require('connect.php'); ?>
 
 <?php
-// $_POST['name']
-// $_POST['year_est']
-$sql = "INSERT INTO brands (name, year_est) VALUES '" . $_POST['name'] . "','" . $_POST['year_est'] . "';";
+$sql = "INSERT INTO brands (name, year_est) VALUES ('" . $_POST['name'] . "', '" . $_POST['year_est'] . "');";
 $mysqli->query($sql);
-headers('Location: brands.php');
+header('Location: brands.php');
+?>
